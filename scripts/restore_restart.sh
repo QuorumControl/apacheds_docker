7baa3b7c241f52ff6ef4373db0f436ecf12a2601
davegraziosi <davide.graziosi@exprivia.it>
2015-03-02 16:25:03 +0100

    more checks on startup + restore script

---
+++ scripts/restore_restart.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+. /root/functions.sh  --source-only
+
+cd /var/lib/apacheds-2.0.0-M19/default
+rm -rf *
+cd /
+tar xvf /backup/backup.tar
+
+/etc/init.d/apacheds-2.0.0-M19-default start
+/etc/init.d/xinetd restart
+
+sleep 30
+RUNNING=`/etc/init.d/apacheds-2.0.0-M19-default status | grep not`
+if [ -n "${RUNNING}" ]; then
+ /etc/init.d/apacheds-2.0.0-M19-default restart
+ sleep 30
+fi
+
+check_login   
+
+check_partition
+
+
+if [ -n "${PARTITION}" ]; then
+    echo Partition already present
+else
+    echo Error Partition not present exiting
+    exit -1    
+fi
+
+enable_replication
+setup_replication
+
+nohup /root/replica_check.sh 0<&- &> /tmp/replica_check.log &
+
+
+/etc/init.d/apacheds-2.0.0-M19-default stop
+/etc/init.d/apacheds-2.0.0-M19-default console
+
