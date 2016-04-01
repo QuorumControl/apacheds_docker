6f4f0560dd4742bf3abca35e10da97f553169635
Renaud <renaudManda@users.noreply.github.com>
2016-04-02 01:17:54 +0200

    bumped apacheds version

--- scripts/restore_restart.sh
+++ scripts/restore_restart.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 . /root/functions.sh  --source-only
 
-cd /var/lib/apacheds-2.0.0-M19/default
+cd /var/lib/apacheds-2.0.0-M21/default
 rm -rf *
 cd /
 tar xvf /backup/backup.tar
 
-/etc/init.d/apacheds-2.0.0-M19-default start
+/etc/init.d/apacheds-2.0.0-M21-default start
 /etc/init.d/xinetd restart
 
 sleep 30
-RUNNING=`/etc/init.d/apacheds-2.0.0-M19-default status | grep not`
+RUNNING=`/etc/init.d/apacheds-2.0.0-M21-default status | grep not`
 if [ -n "${RUNNING}" ]; then
- /etc/init.d/apacheds-2.0.0-M19-default restart
+ /etc/init.d/apacheds-2.0.0-M21-default restart
  sleep 30
 fi
 
@@ -34,6 +34,6 @@ setup_replication
 nohup /root/replica_check.sh 0<&- &> /tmp/replica_check.log &
 
 
-/etc/init.d/apacheds-2.0.0-M19-default stop
-/etc/init.d/apacheds-2.0.0-M19-default console
+/etc/init.d/apacheds-2.0.0-M21-default stop
+/etc/init.d/apacheds-2.0.0-M21-default console
 
